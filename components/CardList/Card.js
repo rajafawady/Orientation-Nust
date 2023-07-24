a0e7b9086812ead3826e96b9b6057fd27e6560b3
Muhammad Suhaib <127488926+msuhaib01@users.noreply.github.com>
2023-07-25 00:27:39 +0500

    basic info printing added

--- components/CardList/Card.js
+++ components/CardList/Card.js
@@ -1,20 +1,23 @@
 import Link from "next/link";
 
-const Card = ({className, children, image, type, id}) => {
-
-    return (
-        <Link href={`/gallery/${type.toLowerCase().replace(/\s/g, '-')}/${id}`}><a>
-            <div
-                style={{
-                    background: `linear-gradient(to top, rgba(114,2,64,0.1), rgba(114,2,64,0.1)), url(${image}) no-repeat top center`,
-                    backgroundSize: 'cover'
-                }}
-                className={`cursor-pointer p-6 w-full h-96 rounded-2xl bg-cover relative overflow-hidden ${className} transition duration-300 ease-in-out`}>
-                <p className='uppercase text-sm text-white'>{type}</p>
-                <h2 className='font-bold text-4xl text-white'>{children}</h2>
-            </div>
-        </a></Link>
-    )
-}
+const Card = ({ className, children, image, type, id, content }) => {
+  return (
+    <Link href={`/gallery/${type.toLowerCase().replace(/\s/g, "-")}/${id}`}>
+      <a>
+        <div
+          style={{
+            background: `linear-gradient(to top, rgba(114,2,64,0.1), rgba(114,2,64,0.1)), url(${image}) no-repeat top center`,
+            backgroundSize: "cover",
+          }}
+          className={`cursor-pointer p-6 w-full h-96 rounded-2xl bg-cover relative overflow-hidden ${className} transition duration-300 ease-in-out`}
+        >
+          <p className="uppercase text-sm text-white">{type}</p>
+          <h2 className="font-bold text-4xl text-white">{children}</h2>
+          <p>{content}</p>
+        </div>
+      </a>
+    </Link>
+  );
+};
 
 export default Card;
--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,20 +1,20 @@
-import gallery from '../../util/gallery.json';
+import gallery from "../../util/gallery.json";
 import CardList from "../../components/CardList/CardList";
-import Layout from '../../components/Layout';
-const Gallery = ({schools, lifeAtNUST}) => {
-    return( 
+import Layout from "../../components/Layout";
+const Gallery = ({ schools, lifeAtNUST }) => {
+  return (
     <Layout title={"Life At NUST"}>
-    <div className='width-container'>
+      <div className="width-container">
         <CardList schools={schools} lifeAtNUST={lifeAtNUST} />
-    </div>
+      </div>
     </Layout>
-    )
-}
+  );
+};
 
 export const getStaticProps = async () => {
-    return {
-        props: gallery
-    }
-}
+  return {
+    props: gallery,
+  };
+};
 
 export default Gallery;
