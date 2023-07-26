e14fa93740484fdcf83dbc7171bac2b64f2cc14d
Uzair Shafi <uzairshaffi654@gmail.com>
2023-07-26 23:47:46 +0500

    Update EC.js

--- components/EC/EC.js
+++ components/EC/EC.js
@@ -9,12 +9,12 @@ export default function EC() {
 
 
       <div className='w-full flex justify-center'>
-        <div className="text-center lg:text-7xl md:text-6xl text-5xl mb-1 font-montserrat my-4 md:mb-20 md:mt-6 text-tyrian-purple dark:text-pale-pink">
+        <div className="text-center lg:text-7xl md:text-6xl text-5xl mb-1 font-heading my-4 md:mb-20 md:mt-6 text-tyrian-purple dark:text-pale-pink">
           <MeetOurTeam placeholderText={[{ type: "heading1", text: "Meet Our Team" },]}/>
         </div>
       </div>
 
-      <div className="text-center lg:text-4xl md:text-4xl text-3xl mb-8 font-montserrat my-4 md:mb-20 md:mt-1 text-tyrian-purple dark:text-pale-pink">
+      <div className="text-center lg:text-4xl md:text-4xl text-3xl mb-8 font-subHeading my-4 md:mb-20 md:mt-1 text-tyrian-purple dark:text-pale-pink">
       <MeetOurTeam placeholderText={[{ type: "heading1", text: "Will Be Revealed Soon..." },]}/>
     </div>
 
