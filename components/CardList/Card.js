a846c3e25090356657e7931a400fb10e1cab9010
Muhammad Suhaib <127488926+msuhaib01@users.noreply.github.com>
2023-07-28 09:51:07 +0500

    new animations done

--- components/CardList/Card.js
+++ components/CardList/Card.js
@@ -10,15 +10,21 @@ const Card = ({ children, image, type, id, content }) => {
             backgroundImage: `url(${image})`,
             backgroundSize: "cover",
             display: "flex",
-            justifyContent: "end",
+            overflow: "auto",
             flexDirection: "column",
+            justifyContent: "space-between",
           }}
-          className={` p-6 h-96 rounded-2xl  overflow-hidden`}
+          className={`h-96 rounded-2xl scrolllock`}
         >
-          <p className="uppercase text-sm text-white">{type}</p>
-          <h2 className="font-bold text-4xl text-white">{children}</h2>
-          <div className="school_link_textMain">
-            <p>{content.split("", 150).join("") + "..."}</p>
+          <div className="school_link_textTop">
+            <div className="px-6 pt-3 uppercase text-sm text-white">{type}</div>
+            <div className="px-6 pb-3 font-bold text-4xl text-white">
+              {children}
+            </div>
+          </div>
+          <div className="px-6 py-3 school_link_textMain">
+            <p>{content.split("", 200).join("") + "..."} </p>
+            <button className="school__button">Learn More</button>
           </div>
         </div>
       </a>
