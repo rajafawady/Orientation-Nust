40f9a1631a66cf5e861a9de822e2a1dc8d1fee98
Ali Abbas <maliabbas2001@gmail.com>
2023-08-01 03:51:22 +0500

    ON station page changed

--- pages/on-station/episode/[id].js
+++ pages/on-station/episode/[id].js
@@ -1,95 +1,80 @@
-import React from "react";
-import videos from "../../../util/episodes.json";
 import { useRouter } from "next/router";
+import React from "react";
+
+import videos from "../../../util/episodes.json";
+
 import Layout from "../../../components/Layout";
-import { GrCaretPrevious, GrCaretNext } from "react-icons/gr";
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCaretLeft,
-  faCaretRight,
-  faSquareCaretLeft,
-  faSquareCaretRight,
-} from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 
 const Episode = () => {
   const router = useRouter();
   const { id } = router.query;
-  const getVideo = () => {
-    return videos.find((vid) => vid.id === id);
-  };
-  const video = getVideo();
+  const currentVideo = videos.find((vid) => vid.id === id);
 
-  const getPrevVideo = () => {
-    const index = videos.findIndex((vid) => vid.id === id);
-    if (index === 0) {
-      return videos[videos.length - 1];
-    }
-    return videos[index - 1];
-  };
-  const prevVideo = getPrevVideo();
-
-  const getNextVideo = () => {
-    const index = videos.findIndex((vid) => vid.id === id);
-    if (index === videos.length - 1) {
-      return videos[0];
-    }
-    return videos[index + 1];
-  };
-  const nextVideo = getNextVideo();
-
-  console.log("video is ", video);
-  console.log("nextVideo is ", nextVideo);
-  console.log("prevVideo is ", prevVideo);
   return (
     <>
-      {video && (
-        <Layout title={`Episode ${video.id}`}>
+      {currentVideo && (
+        <Layout title={`Episode ${currentVideo.id}`}>
           <div className="flex  flex-col items-center mt-20 text-tyrian-purple dark:text-white">
-            <div className="  font-vogue text-5xl sm:text-7xl font-bold ">
+            <h1 className="font-jacklyn text-5xl sm:text-7xl font-bold mb-16">
               ON Station
-            </div>
-            <div className="font-montserrat text-3xl sm:text-5xl pt-2 sm:pt-4">
-              {video.title}
-            </div>
-            <div className="font-sans text-sm sm:text-lg text-center px-4 sm:px-20 py-4 sm:py-8">
-              {video.description}
-            </div>
-            <div className="flex justify-center w-full mb-10 md:mb-20">
-              <div
-                className={`${
-                  video.id === 1 ? "pointer-events-none" : ""
-                }flex px-2`}
-              >
-                <Link href={`/on-station/episode/` + prevVideo.id}>
-                  <div className="my-auto cursor-pointer dark:text-pale-pink text-tyrian-purple  md:px-2  z-3">
-                    <FontAwesomeIcon
-                      icon={faCaretLeft}
-                      className="text-5xl sm:text-7xl md:text-8xl"
-                    />
-                  </div>
-                </Link>
-              </div>
-              <div className="bg-tyrian-purple flex flex-col items-center w-full p-2 sm:p-4">
-                <div className="relative overflow-hidden w-full pt-[56%]">
+            </h1>
+
+            <div className="flex lg:flex-row flex-col justify-center w-full gap-6 px-6">
+              {/* video container */}
+              <div className="flex flex-col gap-1">
+                <div className="relative overflow-hidden w-full aspect-[16/9]">
                   <iframe
-                    src={video.url}
+                    src={currentVideo.url}
                     className="absolute top-0 left-0 bottom-0 right-0 w-full h-full"
                     scrolling="no"
                     allowFullScreen={true}
-                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-                  ></iframe>
+                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share;"
+                  />
                 </div>
+                <h1 className="font-heading text-3xl mt-3">
+                  {currentVideo.title}
+                </h1>
+                <p className="font-body text-sm">{currentVideo.description}</p>
               </div>
-              <div className=" flex px-2">
-                <Link href={`/on-station/episode/` + nextVideo.id}>
-                  <div className="my-auto cursor-pointer dark:text-pale-pink text-tyrian-purple  md:px-2 ">
-                    <FontAwesomeIcon
-                      icon={faCaretRight}
-                      className=" text-5xl sm:text-7xl  md:text-8xl"
-                    />
-                  </div>
-                </Link>
+
+              {/* remaining video container */}
+              <div className="w-full lg:max-w-xs">
+                <h1 className="mb-2 text-2xl font-heading">More Videos</h1>
+                <div className="flex flex-col w-full">
+                  {videos
+                    .filter((video) => video.id !== id)
+                    .map((video) => (
+                      <div
+                        key={video.id}
+                        className="cursor-pointer hover:bg-pale-pink transition-colors w-full"
+                        onClick={() =>
+                          router.push(`/on-station/episode/${video.id}`)
+                        }
+                      >
+                        <div className="max-w-md lg:max-w-xs w-full flex gap-2 items-start p-2">
+                          <div className="relative overflow-hidden w-1/2 aspect-[16/9] ">
+                            <Image
+                              src={video.thumbnailUrl}
+                              // className="absolute top-0 left-0 bottom-0 right-0 w-full h-full"
+                              alt={video.title}
+                              layout="fill"
+                            />
+                          </div>
+
+                          <div className="w-1/2">
+                            <h1 className="text-lg font-subHeading mb-2">
+                              {video.title}
+                            </h1>
+                            <p className="text-xs leading-3 font-body">
+                              {video.description.slice(0, 80)}
+                              {video.description.length > 80 && "..."}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                </div>
               </div>
             </div>
           </div>
