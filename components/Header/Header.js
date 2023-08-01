7e63a667be04f1892439f5fb8d29184ceddcec43
Shameekh Naveed <shameekh2002@gmail.com>
2023-08-01 14:18:30 +0500

    mm

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,30 +1,27 @@
-import React from "react";
-import Music from "../Music/Music";
-import SwitchTheme from "./SwitchTheme";
-import { SidebarNav } from "../Sidebar/Sidebar";
+import React from 'react';
+import Music from '../Music/Music';
+import SwitchTheme from './SwitchTheme';
+import { SidebarNav } from '../Sidebar/Sidebar';
 
-export default function Header({homepage}) {
-  return (
-    <>
-    <div className={`my-header z-10 header bg-transparent md:pl-4 md:pr-4 pt-3 w-full mb-8`}>
-      <SidebarNav />
-    
-      <div className='flex w-full  items-center'>
-        <div className='w-1/3 md:w-3/4'>
+export default function Header({ homepage }) {
+	return (
+		<>
+			<div className={`my-header z-10 header bg-transparent md:pl-4 md:pr-4 pt-3 w-full mb-8`}>
+				{/* <SidebarNav /> */}
 
-        </div>
-       
-        <div className='w-2/3  md:w-1/4 flex justify-end items-center'>
-          <div className=' mr-14 md:pr-5'>
+				<div className='flex w-full  items-center'>
+					<div className='w-1/3 md:w-3/4'></div>
 
-            <Music />
-          </div>
-          <div>
-            <SwitchTheme />
-          </div>
-        </div>
-      </div>
-      </div>
-    </>
-  );
+					<div className='w-2/3  md:w-1/4 flex justify-end items-center'>
+						<div className=' mr-14 md:pr-5'>
+							<Music />
+						</div>
+						<div>
+							<SwitchTheme />
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
--- components/Layout.js
+++ components/Layout.js
@@ -1,38 +1,31 @@
-import { useTheme } from "next-themes";
-import Head from "next/head";
-import React from "react";
-import { useContext } from "react";
-import { ClickContext } from "../context/GlobalProvider";
+import { useTheme } from 'next-themes';
+import Head from 'next/head';
+import React from 'react';
+import { useContext } from 'react';
+import { ClickContext } from '../context/GlobalProvider';
 
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
+import Navbar from './Navbar';
 
-const DynamicHeader = dynamic(() => import("./Header/Header"), {
-  ssr: false,
+const DynamicHeader = dynamic(() => import('./Header/Header'), {
+	ssr: false,
 });
 export default function Layout({ children, title, homePage }) {
-  const { click } = useContext(ClickContext);
-  const { theme, setTheme } = useTheme();
-  return (
-    <div
-      className={`${
-        click & homePage
-          ? theme === "dark"
-            ? "dark-content"
-            : "main-content"
-          : null
-      }`}
-    >
-      <Head>
-        <title>{title}</title>
-      </Head>
-
-
-    
-    <DynamicHeader homepage={homePage}/>
-    
-    
-    {children}
-   
-    </div>
-  );
+	const { click } = useContext(ClickContext);
+	const { theme, setTheme } = useTheme();
+	return (
+		<>
+			<Head>
+				<title>{title}</title>
+			</Head>
+			<div className={`${homePage ? (theme === 'dark' ? 'dark-content' : 'main-content') : null}`}>
+				{/* <div className={`${click & homePage ? (theme === 'dark' ? 'dark-content' : 'main-content') : null}`}> */}
+				<div>
+					<Navbar />
+					{/* <DynamicHeader homepage={homePage}/> */}
+					<div className=''>{children}</div>
+				</div>
+			</div>
+		</>
+	);
 }
