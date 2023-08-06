71a30f4f1b6621f839c8de8a4aba30fb9bb539a5
Shameekh Naveed <shameekh2002@gmail.com>
2023-08-06 18:01:14 +0500

    nm

--- components/CardList/List.js
+++ components/CardList/List.js
@@ -1,84 +1,75 @@
-import Card from "./Card";
-import { useEffect, useState, useRef } from "react";
+import Card from './Card';
+import { useEffect, useState, useRef } from 'react';
 
 const List = ({ list = [], type, gap }) => {
-  const length = list.length;
-  const partLength = Math.ceil(length / 3);
-  const result = [];
+	const length = list.length;
+	const partLength = Math.ceil(length / 3);
+	const result = [];
 
-  const ref = useRef(null);
-  const ref2 = useRef(null);
+	const ref = useRef(null);
+	const ref2 = useRef(null);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (typeof window !== "undefined" && window.innerWidth >= 1025) {
-        const y = window.scrollY;
-        ref.current.style.transform = `translateY(-${y / 100}rem)`;
-        ref2.current.style.transform = `translateY(-${y / 100}rem)`;
-      }
-    };
+	let scrollY = 0;
 
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", handleScroll);
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    }
-  }, []);
+	useEffect(() => {
+		const handleScroll = () => {
+			if (typeof window !== 'undefined' && window.innerWidth >= 1025) {
+				const multiplier = window.scrollY - scrollY > 0 ? 1 : -1;
+				// const y = window.scrollY;
+				console.log(`translateY(${5 * (window.scrollY - scrollY)}rem)`);
+				ref.current.style.transform = `translateY(${0.5 * (window.scrollY - scrollY)}rem)`;
+				ref2.current.style.transform = `translateY(${0.5 * (window.scrollY - scrollY)}rem)`;
+				scrollY = window.scrollY;
+			}
+		};
 
-  for (let i = 0; i < 3; i++) {
-    result.push(list.slice(i * partLength, (i + 1) * partLength));
-  }
+		if (typeof window !== 'undefined') {
+			window.addEventListener('scroll', handleScroll);
+			return () => {
+				window.removeEventListener('scroll', handleScroll);
+			};
+		}
+	}, []);
 
-  return (
-      <>
-        <div className="flex lg:flex-row flex-col gap-4 lg:gap-10">
-          <div className={`flex flex-col ${gap? "pt-44 lg:mt-0" : ""}`} ref={ref}>
-            {result[0].map((data) => (
-                <Card
-                    type={type}
-                    content={data.content}
-                    id={data.id}
-                    key={data.id}
-                    image={data.image}
-                >
-                  {data.name}
-                </Card>
-            ))}
-          </div>
-          <div className={typeof window !== "undefined" && window.innerWidth < 1280 ? "col1" : ""}>
-            <div className="flex flex-col">
-            {result[1].map((data) => (
-                <Card
-                    type={type}
-                    content={data.content}
-                    id={data.id}
-                    key={data.id}
-                    image={data.image}
-                >
-                  {data.name}
-                </Card>
-            ))}
-          </div>
-        </div>
-          <div className={typeof window !== "undefined" && window.innerWidth < 1280 ? "col2" : ""}>
-            <div className={`flex flex-col ${gap? "pt-44 lg:mt-0" : ""}`}  ref={ref2}>
-            {result[2].map((data) => (
-                <Card
-                    type={type}
-                    content={data.content}
-                    id={data.id}
-                    key={data.id}
-                    image={data.image}
-                >
-                  {data.name}
-                </Card>
-            ))}
-            </div>
-          </div>
-        </div>
-      </>
-  );
+	for (let i = 0; i < 3; i++) {
+		result.push(list.slice(i * partLength, (i + 1) * partLength));
+	}
+
+	return (
+		<>
+			<div className='flex lg:flex-row flex-col gap-4 lg:gap-10'>
+				<div
+					className={`flex flex-col transition-transform duration-1000 ${gap ? 'pt-44 lg:mt-0' : ''}`}
+					ref={ref}>
+					{result[0].map((data) => (
+						<Card type={type} content={data.content} id={data.id} key={data.id} image={data.image}>
+							{data.name}
+						</Card>
+					))}
+				</div>
+				<div className={typeof window !== 'undefined' && window.innerWidth < 1280 ? 'col1' : ''}>
+					<div className='flex flex-col transition-transform duration-1000'>
+						{result[1].map((data) => (
+							<Card type={type} content={data.content} id={data.id} key={data.id} image={data.image}>
+								{data.name}
+							</Card>
+						))}
+					</div>
+				</div>
+				<div className={typeof window !== 'undefined' && window.innerWidth < 1280 ? 'col2' : ''}>
+					<div
+						className={`flex flex-col transition-transform duration-1000 ${gap ? 'pt-44 lg:mt-0' : ''}`}
+						ref={ref2}>
+						{result[2].map((data) => (
+							<Card type={type} content={data.content} id={data.id} key={data.id} image={data.image}>
+								{data.name}
+							</Card>
+						))}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default List;
