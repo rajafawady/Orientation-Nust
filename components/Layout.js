f5ca9e304e990e244c4cc7a0b78b89d37fef2853
Ali Abbas <maliabbas2001@gmail.com>
2023-08-08 21:47:24 +0500

    doodles on on-station

--- components/Layout.js
+++ components/Layout.js
@@ -20,7 +20,7 @@ export default function Layout({ children, title, homePage }) {
       <div className="min-h-full">
         <Navbar />
         <div className="w-full flex justify-center">
-          <div className="max-w-xs sm:container">{children}</div>
+          <div className="max-w-md sm:container">{children}</div>
         </div>
       </div>
       <Footer />
--- pages/on-station/episode/[id].js
+++ pages/on-station/episode/[id].js
@@ -5,6 +5,7 @@ import videos from "../../../util/episodes.json";
 
 import Layout from "../../../components/Layout";
 import Image from "next/image";
+import DoodleContainer from "../../../components/DoodleContainer";
 
 const Episode = () => {
   const router = useRouter();
@@ -14,11 +15,20 @@ const Episode = () => {
   return (
     <>
       {currentVideo && (
-        <Layout title={`Episode ${currentVideo.id}`}>
+        <Layout title={`ON Station | Episode ${currentVideo.id}`}>
           <div className="flex  flex-col items-center mt-20  dark:text-white">
-            <h1 className="font-jacklyn text-5xl sm:text-7xl font-bold mb-16 header-1">
-              ON Station
-            </h1>
+            <div className="font-jacklyn text-5xl sm:text-7xl font-bold mb-16 relative">
+              <p className="header-1">ON Station</p>
+
+              <DoodleContainer
+                className={"absolute w-32 aspect-1 -top-10 -left-[40%]"}
+                src="/doodles/on-station/1.png"
+              />
+              <DoodleContainer
+                className={"absolute w-32 aspect-1 -top-10 -right-[40%]"}
+                src="/doodles/on-station/2.png"
+              />
+            </div>
 
             <div className="flex lg:flex-row flex-col justify-center w-full gap-6 px-6 text-tyrian-purple">
               {/* video container */}
@@ -48,7 +58,7 @@ const Episode = () => {
                       className={`cursor-pointer transition-colors w-full
                             ${
                               video.id === id
-                                ? "bg-tyrian-purple text-white"
+                                ? "bg-gradient-to-tl from-dark-purple to-ultra-red text-white"
                                 : "hover:bg-pale-pink"
                             }
                         `}
