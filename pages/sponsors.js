22301c8f449eb081013ea7b4266709956e91074b
msuhaib01 <warheadforehead123@gmail.com>
2023-08-15 16:49:45 +0500

    first draft sponsors page

---
+++ pages/sponsors.js
@@ -0,0 +1,184 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Tabs from "../components/Tabs/Tabs";
+
+export default function donations() {
+  return (
+    <Layout title={"Sponsors"}>
+      <div className="sp-container">
+        <div className="w-full flex items-center justify-center gap-1 md:gap-3 px-3 my-5">
+          <div className="w-1/4 h-[3px] bg-[#464646]"></div>
+          <h1 className="m-14 p-4 shrink-0 plat-heading-text text-4xl">
+            Platinum Sponsors
+          </h1>
+          <div className="w-1/4 h-[3px] bg-[#464646]"></div>
+        </div>
+        {/* <Tabs /> */}
+        <div className="flex justify-center items-center">
+          <div className="mx-12">
+            <h3 className="flex justify-center m-4 p-4 shrink-0 text-4xl ">
+              Google
+            </h3>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              tempus lacus sed orci condimentum pulvinar. Curabitur metus neque,
+              eleifend vitae dui quis, dapibus viverra libero. Donec lacinia ex
+              non consequat tempus. Nam at turpis imperdiet, tincidunt nulla et,
+              congue erat. Proin mollis sed felis eget pretium. Sed tempus nibh
+              nec faucibus ornare. Vivamus id ex eu nulla iaculis interdum.
+              Nulla a interdum odio. Maecenas vel dui lectus. Fusce ullamcorper
+              malesuada eros sit amet posuere. Suspendisse posuere ligula at dui
+              ultricies, sit amet malesuada justo tempor. Aliquam dictum orci
+              vitae facilisis varius.",
+            </p>
+          </div>
+          <div>
+            <div
+              style={{
+                // backgroundColor: "rgba(114,2,64,0.1)",
+                backgroundImage: `url("/sponsor-logos/google.png")`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="rounded mx-12 h-96 w-96 school_link "
+            ></div>
+          </div>
+        </div>
+        <div className="w-full flex items-center justify-center gap-1 md:gap-3 px-3 my-5">
+          <div className="w-1/4 h-[3px] bg-[#ffae00]"></div>
+          <h1 className="m-14 p-4 shrink-0 gold-heading-text text-4xl">
+            Gold Sponsors
+          </h1>
+          <div className="w-1/4 h-[3px] bg-[#ffae00]"></div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="mx-12">
+            <h3 className="flex justify-center m-4 p-4 shrink-0 text-4xl ">
+              Google
+            </h3>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              tempus lacus sed orci condimentum pulvinar. Curabitur metus neque,
+              eleifend vitae dui quis, dapibus viverra libero. Donec lacinia ex
+              non consequat tempus. Nam at turpis imperdiet, tincidunt nulla et,
+              congue erat. Proin mollis sed felis eget pretium. Sed tempus nibh
+              nec faucibus ornare. Vivamus id ex eu nulla iaculis interdum.
+              Nulla a interdum odio. Maecenas vel dui lectus. Fusce ullamcorper
+              malesuada eros sit amet posuere. Suspendisse posuere ligula at dui
+              ultricies, sit amet malesuada justo tempor. Aliquam dictum orci
+              vitae facilisis varius.",
+            </p>
+          </div>
+          <div>
+            <div
+              style={{
+                // backgroundColor: "rgba(114,2,64,0.1)",
+                backgroundImage: `url("/sponsor-logos/google.png")`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="rounded mx-12 h-96 w-96 school_link "
+            ></div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="mx-12">
+            <h3 className="flex justify-center m-4 p-4 shrink-0 text-4xl ">
+              Google
+            </h3>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              tempus lacus sed orci condimentum pulvinar. Curabitur metus neque,
+              eleifend vitae dui quis, dapibus viverra libero. Donec lacinia ex
+              non consequat tempus. Nam at turpis imperdiet, tincidunt nulla et,
+              congue erat. Proin mollis sed felis eget pretium. Sed tempus nibh
+              nec faucibus ornare. Vivamus id ex eu nulla iaculis interdum.
+              Nulla a interdum odio. Maecenas vel dui lectus. Fusce ullamcorper
+              malesuada eros sit amet posuere. Suspendisse posuere ligula at dui
+              ultricies, sit amet malesuada justo tempor. Aliquam dictum orci
+              vitae facilisis varius.",
+            </p>
+          </div>
+          <div>
+            <div
+              style={{
+                // backgroundColor: "rgba(114,2,64,0.1)",
+                backgroundImage: `url("/sponsor-logos/google.png")`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="rounded mx-12 h-96 w-96 school_link "
+            ></div>
+          </div>
+        </div>
+        <div className="w-full flex items-center justify-center gap-1 md:gap-3 px-3 my-5">
+          <div className="w-1/4 h-[3px] bg-[#5123c5]"></div>
+          <h1 className="m-14 p-4 shrink-0 silver-heading-text text-4xl">
+            Silver Sponsors
+          </h1>
+          <div className="w-1/4 h-[3px] bg-[#5123c5]"></div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="mx-12">
+            <h3 className="flex justify-center m-4 p-4 shrink-0 text-4xl ">
+              Google
+            </h3>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              tempus lacus sed orci condimentum pulvinar. Curabitur metus neque,
+              eleifend vitae dui quis, dapibus viverra libero. Donec lacinia ex
+              non consequat tempus. Nam at turpis imperdiet, tincidunt nulla et,
+              congue erat. Proin mollis sed felis eget pretium. Sed tempus nibh
+              nec faucibus ornare. Vivamus id ex eu nulla iaculis interdum.
+              Nulla a interdum odio. Maecenas vel dui lectus. Fusce ullamcorper
+              malesuada eros sit amet posuere. Suspendisse posuere ligula at dui
+              ultricies, sit amet malesuada justo tempor. Aliquam dictum orci
+              vitae facilisis varius.",
+            </p>
+          </div>
+          <div>
+            <div
+              style={{
+                // backgroundColor: "rgba(114,2,64,0.1)",
+                backgroundImage: `url("/sponsor-logos/google.png")`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="rounded mx-12 h-96 w-96 school_link "
+            ></div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="mx-12">
+            <h3 className="flex justify-center m-4 p-4 shrink-0 text-4xl ">
+              Google
+            </h3>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              tempus lacus sed orci condimentum pulvinar. Curabitur metus neque,
+              eleifend vitae dui quis, dapibus viverra libero. Donec lacinia ex
+              non consequat tempus. Nam at turpis imperdiet, tincidunt nulla et,
+              congue erat. Proin mollis sed felis eget pretium. Sed tempus nibh
+              nec faucibus ornare. Vivamus id ex eu nulla iaculis interdum.
+              Nulla a interdum odio. Maecenas vel dui lectus. Fusce ullamcorper
+              malesuada eros sit amet posuere. Suspendisse posuere ligula at dui
+              ultricies, sit amet malesuada justo tempor. Aliquam dictum orci
+              vitae facilisis varius.",
+            </p>
+          </div>
+          <div>
+            <div
+              style={{
+                // backgroundColor: "rgba(114,2,64,0.1)",
+                backgroundImage: `url("/sponsor-logos/google.png")`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="rounded mx-12 h-96 w-96 school_link "
+            ></div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
