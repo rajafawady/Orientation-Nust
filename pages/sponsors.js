7a198b36ab629778b17272131e06e8899ae65a54
msuhaib01 <warheadforehead123@gmail.com>
2023-08-25 23:38:35 +0500

    sponsors added

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -9,34 +9,28 @@ export default function donations() {
         <div className="w-full flex items-center justify-center gap-1 md:gap-3 px-3 my-5">
           <div className="w-1/4 h-[3px] bg-[#464646]"></div>
           <h1 className="m-14 p-4 shrink-0 plat-heading-text text-4xl">
-            Platinum Sponsors
+            Winterland
           </h1>
           <div className="w-1/4 h-[3px] bg-[#464646]"></div>
         </div>
-        {/* <Tabs /> */}
         <div className="flex justify-center items-center">
           <div className="mx-12">
-            <h3 className="flex justify-center m-2 p-4 shrink-0 text-4xl ">
-              Google
-            </h3>
             <p className="text-justify">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              tempus lacus sed orci condimentum pulvinar. Curabitur metus neque,
-              eleifend vitae dui quis, dapibus viverra libero. Donec lacinia ex
-              non consequat tempus. Nam at turpis imperdiet, tincidunt nulla et,
-              congue erat. Proin mollis sed felis eget pretium. Sed tempus nibh
-              nec faucibus ornare. Vivamus id ex eu nulla iaculis interdum.
-              Nulla a interdum odio. Maecenas vel dui lectus. Fusce ullamcorper
-              malesuada eros sit amet posuere. Suspendisse posuere ligula at dui
-              ultricies, sit amet malesuada justo tempor. Aliquam dictum orci
-              vitae facilisis varius.",
+              Winterland is Pakistan’s pioneering snow park, offering an
+              immersive & enchanting experience of winter for its visitors. The
+              park provides a unique opportunity for individuals and families to
+              indulge in the joys of a winter wonderland with a variety of
+              activities such as ice skating, snow tubing, ice cycling &
+              snowball fights, all within a controlled indoor environment. With
+              branches in both Lahore and Karachi, Winterland is a destination
+              for those seeking a taste of winter magic.
             </p>
           </div>
           <div>
             <div
               style={{
                 // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/google.png")`,
+                backgroundImage: `url("/sponsor-logos/winterland.png")`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
@@ -47,46 +41,16 @@ export default function donations() {
         <div className="w-full flex items-center justify-center gap-1 md:gap-3 px-3 my-5">
           <div className="w-1/4 h-[3px] bg-[#ffae00]"></div>
           <h1 className="m-14 p-4 shrink-0 gold-heading-text text-4xl">
-            Gold Sponsors
+            Vouch365
           </h1>
           <div className="w-1/4 h-[3px] bg-[#ffae00]"></div>
         </div>
         <div className="flex justify-center items-center">
-          <div className="mx-12">
-            <h3 className="flex justify-center m-2 p-4 shrink-0 text-4xl ">
-              Google
-            </h3>
-            <p className="text-justify">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              tempus lacus sed orci condimentum pulvinar. Curabitur metus neque,
-              eleifend vitae dui quis, dapibus viverra libero. Donec lacinia ex
-              non consequat tempus. Nam at turpis imperdiet, tincidunt nulla et,
-              congue erat. Proin mollis sed felis eget pretium. Sed tempus nibh
-              nec faucibus ornare. Vivamus id ex eu nulla iaculis interdum.
-              Nulla a interdum odio. Maecenas vel dui lectus. Fusce ullamcorper
-              malesuada eros sit amet posuere. Suspendisse posuere ligula at dui
-              ultricies, sit amet malesuada justo tempor. Aliquam dictum orci
-              vitae facilisis varius.",
-            </p>
-          </div>
           <div>
             <div
               style={{
                 // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/google.png")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-              className="rounded mx-12 h-72 w-72 school_link "
-            ></div>
-          </div>
-        </div>
-        <div className="flex justify-center items-center">
-          <div>
-            <div
-              style={{
-                // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/google.png")`,
+                backgroundImage: `url("/sponsor-logos/vouch.png")`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
@@ -94,88 +58,118 @@ export default function donations() {
             ></div>
           </div>
           <div className="mx-12">
-            <h3 className="flex justify-center m-2 p-4 shrink-0 text-4xl ">
-              Google
-            </h3>
             <p className="text-justify">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              tempus lacus sed orci condimentum pulvinar. Curabitur metus neque,
-              eleifend vitae dui quis, dapibus viverra libero. Donec lacinia ex
-              non consequat tempus. Nam at turpis imperdiet, tincidunt nulla et,
-              congue erat. Proin mollis sed felis eget pretium. Sed tempus nibh
-              nec faucibus ornare. Vivamus id ex eu nulla iaculis interdum.
-              Nulla a interdum odio. Maecenas vel dui lectus. Fusce ullamcorper
-              malesuada eros sit amet posuere. Suspendisse posuere ligula at dui
-              ultricies, sit amet malesuada justo tempor. Aliquam dictum orci
-              vitae facilisis varius.",
+              Vouch365 is an application that have access to 14 cities including
+              major cities like (karachi, lahore, islamabad) They are proving
+              10,000+ buy one get one free offers on your favourite brands
+              (Ramada Hotel, Broadway Pizza, IHOP, Dunkin, California Pizza,
+              Cinnabon, Ginsoy Extreme, Movenpick Hotel) in just 999 only! So
+              what are you waiting for, Order now! Save Now!{" "}
             </p>
           </div>
         </div>
         <div className="w-full flex items-center justify-center gap-1 md:gap-3 px-3 my-5">
           <div className="w-1/4 h-[3px] bg-[#5123c5]"></div>
           <h1 className="m-14 p-4 shrink-0 silver-heading-text text-4xl">
-            Silver Sponsors
+            Clicky
           </h1>
           <div className="w-1/4 h-[3px] bg-[#5123c5]"></div>
         </div>
         <div className="flex justify-center items-center">
           <div className="mx-12">
-            <h3 className="flex justify-center m-2 p-4 shrink-0 text-4xl ">
-              Google
-            </h3>
             <p className="text-justify">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              tempus lacus sed orci condimentum pulvinar. Curabitur metus neque,
-              eleifend vitae dui quis, dapibus viverra libero. Donec lacinia ex
-              non consequat tempus. Nam at turpis imperdiet, tincidunt nulla et,
-              congue erat. Proin mollis sed felis eget pretium. Sed tempus nibh
-              nec faucibus ornare. Vivamus id ex eu nulla iaculis interdum.
-              Nulla a interdum odio. Maecenas vel dui lectus. Fusce ullamcorper
-              malesuada eros sit amet posuere. Suspendisse posuere ligula at dui
-              ultricies, sit amet malesuada justo tempor. Aliquam dictum orci
-              vitae facilisis varius.",
+              Clicky.pk stands as a distinguished online fashion platform in
+              Pakistan, exclusively catering to the style needs of women, men,
+              and kids. Our platform offers a comprehensive range of fashion
+              essentials, including clothing, accessories, and footwear, all
+              crafted with impeccable quality to satisfy customers across the
+              nation. With a diverse collection, we proudly showcase both
+              top-tier national and international brands, making us a preferred
+              choice for fashion enthusiasts. <br />
+              We take pride in being a premier e-commerce destination that not
+              only offers a wide array of products but also supports other
+              sellers in their endeavors. Our dedicated customer service team,
+              skilled graphic designers, agile developers, and adept writers
+              collectively contribute to establishing the robust foundation of
+              Clicky.pk. Our commitment to excellence is reflected through the
+              following services:
+              <br />
+              <br />
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}• A vibrant and extensive assortment of fashion
+              products.
+              <br />
+              <br />
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}• Swift and reliable delivery services to customers'
+              doorsteps.
+              <br />
+              <br />
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}• Placing utmost priority on addressing and resolving
+              client concerns.
+              <br />
+              <br />
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}• Providing clear and comprehensive product
+              descriptions, highlighting key features.
+              <br />
+              <br />
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}• Offering a user-friendly browsing experience, ensuring
+              ease of navigation.
+              <br />
+              <br />
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}• Guiding and assisting customers throughout the
+              purchasing process.
+              <br />
+              <br />
+              At Clicky.pk, we are not only a fashion destination but also a
+              reliable partner in your shopping journey, dedicated to delivering
+              quality, style, and outstanding customer service.
             </p>
           </div>
           <div>
             <div
               style={{
                 // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/google.png")`,
+                backgroundImage: `url("/sponsor-logos/clicky.png")`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
               className="rounded mx-12 h-72 w-72 school_link "
             ></div>
-          </div>
-        </div>
-        <div className="flex justify-center items-center">
-          <div>
-            <div
-              style={{
-                // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/google.png")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-              className="rounded mx-12 h-72 w-72 school_link "
-            ></div>
-          </div>
-          <div className="mx-12">
-            <h3 className="flex justify-center m-2 p-4 shrink-0 text-4xl ">
-              Google
-            </h3>
-            <p className="text-justify">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              tempus lacus sed orci condimentum pulvinar. Curabitur metus neque,
-              eleifend vitae dui quis, dapibus viverra libero. Donec lacinia ex
-              non consequat tempus. Nam at turpis imperdiet, tincidunt nulla et,
-              congue erat. Proin mollis sed felis eget pretium. Sed tempus nibh
-              nec faucibus ornare. Vivamus id ex eu nulla iaculis interdum.
-              Nulla a interdum odio. Maecenas vel dui lectus. Fusce ullamcorper
-              malesuada eros sit amet posuere. Suspendisse posuere ligula at dui
-              ultricies, sit amet malesuada justo tempor. Aliquam dictum orci
-              vitae facilisis varius.",
-            </p>
           </div>
         </div>
       </div>
