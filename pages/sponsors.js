05ead97034e47403eee142b90e416029a75f70e1
Shameekh Naveed <shameekh2002@gmail.com>
2023-09-09 22:17:32 +0500

    sponsors responsive

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -1,203 +1,174 @@
-import React from "react";
-import Layout from "../components/Layout";
-import Tabs from "../components/Tabs/Tabs";
+import React from 'react';
+import Layout from '../components/Layout';
+import Tabs from '../components/Tabs/Tabs';
 
 export default function donations() {
-  return (
-    <Layout title={"Sponsors"}>
-      <div className="w-full flex flex-col">
-        <div className="w-full flex items-center justify-center gap-1 px-3 flex-col lg:flex-row mt-5">
-          <div className="w-1/4 h-[6px] bg-[#3f2073]"></div>
-          <h1 className="m-5 lg:mx-14 p-1 lg:p-4 shrink-0  sp-heading text-6xl">
-            Our Sponsors
-          </h1>
-          <div className="w-1/4 h-[6px] bg-[#3f2073]"></div>
-        </div>
-        <div className="w-full flex items-center justify-center gap-1 px-3 my-2 flex-col lg:flex-row">
-          <div className="w-1/4 h-[3px] bg-[#5123c5]"></div>
-          <h1 className="m-5 lg:m-14 p-1 lg:p-4 shrink-0  silver-heading-text text-4xl">
-            Winterland
-          </h1>
-          <div className="w-1/4 h-[3px] bg-[#5123c5]"></div>
-        </div>
-        <div className="flex justify-center items-center flex-col lg:flex-row">
-          <div className="mx-2 lg:mx-12">
-            <p className="text-justify">
-              Winterland is Pakistan’s pioneering snow park, offering an
-              immersive & enchanting experience of winter for its visitors. The
-              park provides a unique opportunity for individuals and families to
-              indulge in the joys of a winter wonderland with a variety of
-              activities such as ice skating, snow tubing, ice cycling &
-              snowball fights, all within a controlled indoor environment. With
-              branches in both Lahore and Karachi, Winterland is a destination
-              for those seeking a taste of winter magic.
-            </p>
-          </div>
-          <div>
-            <div
-              style={{
-                // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/winterland.png")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-              className="rounded mx-2 lg:mx-12 h-72 w-72 school_link "
-            ></div>
-          </div>
-        </div>
-        <div className="w-full flex items-center justify-center gap-1 px-3 my-2 flex-col lg:flex-row">
-          <div className="w-1/4 h-[3px] bg-[#ffae00]"></div>
-          <h1 className="m-5 lg:m-14 p-1 lg:p-4 shrink-0  gold-heading-text text-4xl">
-            Vouch365
-          </h1>
-          <div className="w-1/4 h-[3px] bg-[#ffae00]"></div>
-        </div>
-        <div className="flex justify-center items-center flex-col lg:flex-row">
-          <div>
-            <div
-              style={{
-                // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/vouch.png")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-              className="rounded mx-2 lg:mx-12 h-72 w-72 school_link "
-            ></div>
-          </div>
-          <div className="mx-2 lg:mx-12">
-            <p className="text-justify">
-              Vouch365 is an application that have access to 14 cities including
-              major cities like (karachi, lahore, islamabad) They are proving
-              10,000+ buy one get one free offers on your favourite brands
-              (Ramada Hotel, Broadway Pizza, IHOP, Dunkin, California Pizza,
-              Cinnabon, Ginsoy Extreme, Movenpick Hotel) in just 999 only! So
-              what are you waiting for, Order now! Save Now!{" "}
-            </p>
-          </div>
-        </div>
-        <div className="w-full flex items-center justify-center gap-1 px-3 my-2 flex-col lg:flex-row">
-          <div className="w-1/4 h-[3px] bg-[#464646]"></div>
-          <h1 className="m-5 lg:m-14 p-1 lg:p-4 shrink-0  plat-heading-text text-4xl">
-            Clicky
-          </h1>
-          <div className="w-1/4 h-[3px] bg-[#464646]"></div>
-        </div>
-        <div className="flex justify-center items-center flex-col lg:flex-row">
-          <div className="mx-2 lg:mx-12">
-            <p className="text-justify">
-              Clicky.pk stands as a distinguished online fashion platform in
-              Pakistan, exclusively catering to the style needs of women, men,
-              and kids. Our platform offers a comprehensive range of fashion
-              essentials, including clothing, accessories, and footwear, all
-              crafted with impeccable quality to satisfy customers across the
-              nation. With a diverse collection, we proudly showcase both
-              top-tier national and international brands, making us a preferred
-              choice for fashion enthusiasts. <br />
-              We take pride in being a premier e-commerce destination that not
-              only offers a wide array of products but also supports other
-              sellers in their endeavors. Our dedicated customer service team,
-              skilled graphic designers, agile developers, and adept writers
-              collectively contribute to establishing the robust foundation of
-              Clicky.pk. Our commitment to excellence is reflected through the
-              following services:
-              <br />
-              <br />
-              {"\u00A0"}
-              {"\u00A0"}
-              {"\u00A0"}• A vibrant and extensive assortment of fashion
-              products.
-              <br />
-              <br />
-              {"\u00A0"}
-              {"\u00A0"}
-              {"\u00A0"}• Swift and reliable delivery services to customers'
-              doorsteps.
-              <br />
-              <br />
-              {"\u00A0"}
-              {"\u00A0"}
-              {"\u00A0"}• Placing utmost priority on addressing and resolving
-              client concerns.
-              <br />
-              <br />
-              {"\u00A0"}
-              {"\u00A0"}
-              {"\u00A0"}• Providing clear and comprehensive product
-              descriptions, highlighting key features.
-              <br />
-              <br />
-              {"\u00A0"}
-              {"\u00A0"}
-              {"\u00A0"}• Offering a user-friendly browsing experience, ensuring
-              ease of navigation.
-              <br />
-              <br />
-              {"\u00A0"}
-              {"\u00A0"}
-              {"\u00A0"}• Guiding and assisting customers throughout the
-              purchasing process.
-              <br />
-              <br />
-              At Clicky.pk, we are not only a fashion destination but also a
-              reliable partner in your shopping journey, dedicated to delivering
-              quality, style, and outstanding customer service.
-            </p>
-          </div>
-          <div>
-            <div
-              style={{
-                // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/clicky.png")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-              className="rounded mx-2 lg:mx-12 h-72 w-72 school_link "
-            ></div>
-          </div>
-        </div>
-        <div className="w-full flex items-center justify-center gap-1 px-3 my-2 flex-col lg:flex-row">
-          <div className="w-1/4 h-[3px] bg-[#5123c5]"></div>
-          <h1 className="m-5 lg:m-14 p-1 lg:p-4 shrink-0  silver-heading-text text-4xl">
-            More Sponsors
-          </h1>
-          <div className="w-1/4 h-[3px] bg-[#5123c5]"></div>
-        </div>
-        <div className="flex justify-center">
-          <div>
-            <div
-              style={{
-                // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/breeo.png")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-              className="rounded mx-2 lg:mx-12 h-72 w-72 school_link "
-            ></div>
-          </div>
-          <div>
-            <div
-              style={{
-                // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/cheezious.png")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-              className="rounded mx-2 lg:mx-12 h-72 w-72 school_link "
-            ></div>
-          </div>
-          <div>
-            <div
-              style={{
-                // backgroundColor: "rgba(114,2,64,0.1)",
-                backgroundImage: `url("/sponsor-logos/reach.png")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-              className="rounded mx-2 lg:mx-12 h-72 w-72 school_link "
-            ></div>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
+	return (
+		<Layout title={'Sponsors'}>
+			<div className='w-full flex flex-col'>
+				<div className='w-full flex items-center justify-center gap-1 px-3 flex-col lg:flex-row mt-5'>
+					<div className='w-1/4 h-[6px] bg-[#3f2073]'></div>
+					<h1 className='m-5 lg:mx-14 p-1 lg:p-4 shrink-0  sp-heading text-6xl'>Our Sponsors</h1>
+					<div className='w-1/4 h-[6px] bg-[#3f2073]'></div>
+				</div>
+				<div className='w-full flex items-center justify-center gap-1 px-3 my-2 flex-col lg:flex-row'>
+					<div className='w-1/4 h-[3px] bg-[#5123c5]'></div>
+					<h1 className='m-5 lg:m-14 p-1 lg:p-4 shrink-0  silver-heading-text text-4xl'>Winterland</h1>
+					<div className='w-1/4 h-[3px] bg-[#5123c5]'></div>
+				</div>
+				<div className='flex justify-center items-center flex-col lg:flex-row'>
+					<div className='mx-2 lg:mx-12'>
+						<p className='text-justify'>
+							Winterland is Pakistan’s pioneering snow park, offering an immersive & enchanting experience
+							of winter for its visitors. The park provides a unique opportunity for individuals and
+							families to indulge in the joys of a winter wonderland with a variety of activities such as
+							ice skating, snow tubing, ice cycling & snowball fights, all within a controlled indoor
+							environment. With branches in both Lahore and Karachi, Winterland is a destination for those
+							seeking a taste of winter magic.
+						</p>
+					</div>
+					<div>
+						<div
+							style={{
+								// backgroundColor: "rgba(114,2,64,0.1)",
+								backgroundImage: `url("/sponsor-logos/winterland.png")`,
+								backgroundSize: 'cover',
+								backgroundPosition: 'center',
+							}}
+							className='rounded mx-2 lg:mx-12 h-72 w-72 school_link '></div>
+					</div>
+				</div>
+				<div className='w-full flex items-center justify-center gap-1 px-3 my-2 flex-col lg:flex-row'>
+					<div className='w-1/4 h-[3px] bg-[#ffae00]'></div>
+					<h1 className='m-5 lg:m-14 p-1 lg:p-4 shrink-0  gold-heading-text text-4xl'>Vouch365</h1>
+					<div className='w-1/4 h-[3px] bg-[#ffae00]'></div>
+				</div>
+				<div className='flex justify-center items-center flex-col lg:flex-row'>
+					<div>
+						<div
+							style={{
+								// backgroundColor: "rgba(114,2,64,0.1)",
+								backgroundImage: `url("/sponsor-logos/vouch.png")`,
+								backgroundSize: 'cover',
+								backgroundPosition: 'center',
+							}}
+							className='rounded mx-2 lg:mx-12 h-72 w-72 school_link '></div>
+					</div>
+					<div className='mx-2 lg:mx-12'>
+						<p className='text-justify'>
+							Vouch365 is an application that have access to 14 cities including major cities like
+							(karachi, lahore, islamabad) They are proving 10,000+ buy one get one free offers on your
+							favourite brands (Ramada Hotel, Broadway Pizza, IHOP, Dunkin, California Pizza, Cinnabon,
+							Ginsoy Extreme, Movenpick Hotel) in just 999 only! So what are you waiting for, Order now!
+							Save Now!{' '}
+						</p>
+					</div>
+				</div>
+				<div className='w-full flex items-center justify-center gap-1 px-3 my-2 flex-col lg:flex-row'>
+					<div className='w-1/4 h-[3px] bg-[#464646]'></div>
+					<h1 className='m-5 lg:m-14 p-1 lg:p-4 shrink-0  plat-heading-text text-4xl'>Clicky</h1>
+					<div className='w-1/4 h-[3px] bg-[#464646]'></div>
+				</div>
+				<div className='flex justify-center items-center flex-col lg:flex-row'>
+					<div className='mx-2 lg:mx-12'>
+						<p className='text-justify'>
+							Clicky.pk stands as a distinguished online fashion platform in Pakistan, exclusively
+							catering to the style needs of women, men, and kids. Our platform offers a comprehensive
+							range of fashion essentials, including clothing, accessories, and footwear, all crafted with
+							impeccable quality to satisfy customers across the nation. With a diverse collection, we
+							proudly showcase both top-tier national and international brands, making us a preferred
+							choice for fashion enthusiasts. <br />
+							We take pride in being a premier e-commerce destination that not only offers a wide array of
+							products but also supports other sellers in their endeavors. Our dedicated customer service
+							team, skilled graphic designers, agile developers, and adept writers collectively contribute
+							to establishing the robust foundation of Clicky.pk. Our commitment to excellence is
+							reflected through the following services:
+							<br />
+							<br />
+							{'\u00A0'}
+							{'\u00A0'}
+							{'\u00A0'}• A vibrant and extensive assortment of fashion products.
+							<br />
+							<br />
+							{'\u00A0'}
+							{'\u00A0'}
+							{'\u00A0'}• Swift and reliable delivery services to customers' doorsteps.
+							<br />
+							<br />
+							{'\u00A0'}
+							{'\u00A0'}
+							{'\u00A0'}• Placing utmost priority on addressing and resolving client concerns.
+							<br />
+							<br />
+							{'\u00A0'}
+							{'\u00A0'}
+							{'\u00A0'}• Providing clear and comprehensive product descriptions, highlighting key
+							features.
+							<br />
+							<br />
+							{'\u00A0'}
+							{'\u00A0'}
+							{'\u00A0'}• Offering a user-friendly browsing experience, ensuring ease of navigation.
+							<br />
+							<br />
+							{'\u00A0'}
+							{'\u00A0'}
+							{'\u00A0'}• Guiding and assisting customers throughout the purchasing process.
+							<br />
+							<br />
+							At Clicky.pk, we are not only a fashion destination but also a reliable partner in your
+							shopping journey, dedicated to delivering quality, style, and outstanding customer service.
+						</p>
+					</div>
+					<div>
+						<div
+							style={{
+								// backgroundColor: "rgba(114,2,64,0.1)",
+								backgroundImage: `url("/sponsor-logos/clicky.png")`,
+								backgroundSize: 'cover',
+								backgroundPosition: 'center',
+							}}
+							className='rounded mx-2 lg:mx-12 h-72 w-72 school_link '></div>
+					</div>
+				</div>
+				<div className='w-full flex items-center justify-center gap-1 px-3 my-2 flex-col lg:flex-row'>
+					<div className='w-1/4 h-[3px] bg-[#5123c5]'></div>
+					<h1 className='m-5 lg:m-14 p-1 lg:p-4 shrink-0  silver-heading-text text-4xl'>More Sponsors</h1>
+					<div className='w-1/4 h-[3px] bg-[#5123c5]'></div>
+				</div>
+				<div className='flex justify-center flex-wrap'>
+					<div>
+						<div
+							style={{
+								// backgroundColor: "rgba(114,2,64,0.1)",
+								backgroundImage: `url("/sponsor-logos/breeo.png")`,
+								backgroundSize: 'cover',
+								backgroundPosition: 'center',
+							}}
+							className='rounded mx-2 lg:mx-12 h-72 w-72 school_link '></div>
+					</div>
+					<div>
+						<div
+							style={{
+								// backgroundColor: "rgba(114,2,64,0.1)",
+								backgroundImage: `url("/sponsor-logos/cheezious.png")`,
+								backgroundSize: 'cover',
+								backgroundPosition: 'center',
+							}}
+							className='rounded mx-2 lg:mx-12 h-72 w-72 school_link '></div>
+					</div>
+					<div>
+						<div
+							style={{
+								// backgroundColor: "rgba(114,2,64,0.1)",
+								backgroundImage: `url("/sponsor-logos/reach.png")`,
+								backgroundSize: 'cover',
+								backgroundPosition: 'center',
+							}}
+							className='rounded mx-2 lg:mx-12 h-72 w-72 school_link '></div>
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
 }
