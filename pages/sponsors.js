a3fd1f7b7441213bcc9c01223fc5a9f42fd89d98
naumanch969 <naumanch969@gmail.com>
2024-09-02 00:15:47 +0500

    Sponsors update

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
-export default function donations() {
+export default function Donations() {
 	return (
 		<Layout title={'Sponsors'}>
 			<div className='w-full flex flex-col'>
@@ -11,88 +11,86 @@ export default function donations() {
 					<div className='w-1/4 h-[6px] bg-coral'></div>
 				</div>
 
-				<div className='flex justify-center flex-wrap gap-4 mt-10'>
+				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
+					
 					<div
 						style={{
-							backgroundImage: `url("/sponsor-logos/AEO.jpg")`,
+							backgroundImage: url("/sponsor-logos/California.png"),
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}}
+						className='rounded h-72 w-72 mx-auto school_link'></div>
+					<div
+						style={{
+							backgroundImage: url("/sponsor-logos/cheezious.png"),
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}}
+						className='rounded h-72 w-72 mx-auto school_link'></div>
+					<div
+						style={{
+							backgroundImage: url("/sponsor-logos/crusteez.png"),
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}}
+						className='rounded h-72 w-72 mx-auto school_link'></div>
+					<div
+						style={{
+							backgroundImage: url("/sponsor-logos/nearpear.jpg"),
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}}
+						className='rounded h-72 w-72 mx-auto school_link'></div>
+					<div
+						style={{
+							backgroundImage: url("/sponsor-logos/orange lake .png"),
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}}
+						className='rounded h-72 w-72 mx-auto school_link'></div>
+					<div
+						style={{
+							backgroundImage: url("/sponsor-logos/Pizzrela.jpg"),
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}}
+						className='rounded h-72 w-72 mx-auto school_link'></div>
+					<div
+						style={{
+							backgroundImage: url("/sponsor-logos/Ranchers .png"),
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}}
+						className='rounded h-72 w-72 mx-auto school_link'></div>
+					<div
+						style={{
+							backgroundImage: url("/sponsor-logos/ROX.jpg"),
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}}
+						className='rounded h-72 w-72 mx-auto school_link'></div>
+					<div
+						style={{
+							backgroundImage: url("/sponsor-logos/USA.jpg"),
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}}
+						className='rounded h-72 w-72 mx-auto school_link'></div>
+						<div
+						style={{
+							backgroundImage: url("/sponsor-logos/AEO.jpg"),
 							backgroundSize: 'cover',
 							backgroundPosition: 'center'
 						}}
-						className='rounded h-72 w-72 school_link'></div>
+						className='rounded h-72 w-72 mx-auto school_link'></div>
 					<div
 						style={{
-							backgroundImage: `url("/sponsor-logos/ASCOM.jpg")`,
+							backgroundImage: url("/sponsor-logos/ASCOM.jpg"),
 							backgroundSize: 'cover',
 							backgroundPosition: 'center',
 						}}
-						className='rounded h-72 w-72 school_link'></div>
-					<div
-						style={{
-							backgroundImage: `url("/sponsor-logos/California.png")`,
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-						}}
-						className='rounded h-72 w-72 school_link'></div>
-					<div
-						style={{
-							backgroundImage: `url("/sponsor-logos/cheezious.png")`,
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-						}}
-						className='rounded h-72 w-72 school_link'></div>
-					<div
-						style={{
-							backgroundImage: `url("/sponsor-logos/crusteez.png")`,
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-						}}
-						className='rounded h-72 w-72 school_link'></div>
-						<div
-						style={{
-							backgroundImage: `url("/sponsor-logos/nearpear.jpg")`,
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-						}}
-						className='rounded h-72 w-72 school_link'></div>
-						<div
-						style={{
-							backgroundImage: `url("/sponsor-logos/orange lake .png")`,
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-						}}
-						className='rounded h-72 w-72 school_link'></div>
-						<div
-						style={{
-							backgroundImage: `url("/sponsor-logos/Pizzrela.jpg")`,
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-						}}
-						className='rounded h-72 w-72 school_link'></div>
-						<div
-						style={{
-							backgroundImage: `url("/sponsor-logos/Ranchers .png")`,
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-						}}
-						className='rounded h-72 w-72 school_link'></div>
-						
-						<div
-						style={{
-							backgroundImage: `url("/sponsor-logos/ROX.jpg")`,
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-						}}
-						className='rounded h-72 w-72 school_link'></div>
-						<div
-						style={{
-							backgroundImage: `url("/sponsor-logos/USA.jpg")`,
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-						}}
-						className='rounded h-72 w-72 school_link'></div>
-						
+						className='rounded h-72 w-72 mx-auto school_link'></div>
 				</div>
-				
 			</div>
 		</Layout>
 	);
