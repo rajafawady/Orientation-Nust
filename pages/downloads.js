0626839b66ed63138e7636e6908f5c9f9db99ab1
Raja Fawad <rajafawady@gmail.com>
2024-09-12 21:34:10 +0500

    Update downloads.js
    Corrected picture names

--- pages/downloads.js
+++ pages/downloads.js
@@ -13,11 +13,11 @@ const Downloads = () => {
 				</div>
 				<div className='flex w-full  flex-wrap justify-evenly'>
 					<BatchImage imgName='day-photo-1.jpg' dlName='day-photo-1' btnText='Day Drone' />
-					<BatchImage imgName='night-photo-1.png' dlName='night-photo-1' btnText='Night Drone' />
+					<BatchImage imgName='night-photo-1.jpg' dlName='night-photo-1' btnText='Night Drone' />
 					<BatchImage imgName='day-photo-2.jpg' dlName='day-photo-2' btnText='Day Panorama' />
-					<BatchImage imgName='night-photo-2.png' dlName='night-photo-2' btnText='Night Panorama' />
+					<BatchImage imgName='night-photo-2.jpg' dlName='night-photo-2' btnText='Night Panorama' />
 					<BatchImage imgName='day-photo-3.jpg' dlName='day-photo-3' btnText='Daytime Aerial' />
-					<BatchImage imgName='night-photo-3.png' dlName='night-photo-3' btnText='Night Aerial' />
+					<BatchImage imgName='night-photo-3.jpg' dlName='night-photo-3' btnText='Night Aerial' />
 				</div>
 			</div>
 		</Layout>
